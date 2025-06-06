9131ff77366ee2dc06b6168611fa6df16c04ed6c
SEC <yogayazhini07@gmail.com>
2025-06-06 14:14:36 +0530

    leave balancemanagement craeted

--- src/main/java/com/saveetha/LeaveManagement/LeaveManagemnetApplication.java
+++ src/main/java/com/saveetha/LeaveManagement/LeaveManagemnetApplication.java
@@ -7,10 +7,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 
 @EnableScheduling
 @SpringBootApplication
-public class
-LeaveManagemnetApplication {
-
-
+public class LeaveManagemnetApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LeaveManagemnetApplication.class, args);
 		System.out.println("hello yoga");
